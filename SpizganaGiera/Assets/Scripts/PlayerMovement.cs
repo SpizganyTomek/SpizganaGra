e3f8a657aabd0d3016e037bbf4fb4b88ded1ec34
Maagar <magar123321@gmail.com>
2020-08-11 16:44:10 +0200

    Movement+animations
    Podstawy fizyki, skakanie, animacje chodzenia i skakania. Tymczasowa postać orc, która jest animowana szkieletowo + platforma do testów skakania. Dodatkowo skrypt "BetterJumping", dzięki któremu skoki zależą od długości wciśnięcia przycisku skoku i postać opada szybciej niż się wznosi co daje przyjemniejszy efekt.

---
+++ SpizganaGiera/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("isjumping", true);
+        }
+    }
+
+    public void OnLanding ()
+    {
+        animator.SetBool("isjumping", false);
+    }
+
+
+    void FixedUpdate()
+    {
+        controller.Move(0f, false, jump);
+        jump = false;
+    }
+
+}
